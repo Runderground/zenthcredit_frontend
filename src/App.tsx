db8f7a44220ac391f4194f24a121813f94efccfc
Rafael Bueno <78382141+Runderground@users.noreply.github.com>
2024-11-07 15:45:07 +0000

    Removendo guia do Git

--- src/App.tsx
+++ src/App.tsx
@@ -33,10 +33,3 @@ function App() {
 }
 
 export default App
-
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin https://github.com/Runderground/zenthcredit_frontend.git
-// git push -u origin main
