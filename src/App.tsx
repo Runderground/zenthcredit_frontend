8f79030044e4f87f2a1309900ed9cc48f8e0f495
Dog1337s <anacarolinamarketdigital@gmail.com>
2024-11-07 20:03:04 +0000

    Add: added first section

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Home from './pages/Home'
 function App() {
   return (
     <BrowserRouter>
-      <main>
+      <main className="w-full h-full">
         <Routes>
           <Route path="/" element={<Home/>} />
         </Routes>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,24 @@
 import HomeSvg from '../assets/Home_Section_One.svg'
-
+import {Button} from '@/components/ui/button'
 
 export default function Home() {
   return (
-    <section className="flex flex-col md:flex-row">
-      <div>
-        <h1 className="text-5xl font-bold text-slate-800">Simule seu empréstimo com a Zenith</h1>
-        <p>Com o nosso sistema, você poderá realizar simulações de empréstimo direto no site! Após isso poderá falar com algum de nossos consultores.</p>
-      </div>
-      <img className="md:w-1/2 md:h-1/2" src={HomeSvg} alt="Home SVG" height={400} width={400}/>
+      <>
+        {/* Sessão 1 */}
+      <section className="m-4 flex flex-col lg:flex-row items-center lg:items-start justify-center lg:justify-start text-center lg:text-start">
+        <div>
+          <h1 className="text-5xl lg:text-6xl font-bold text-zinc-800 mb-2">Simule seu empréstimo com a Zenith Credit</h1>
+          <p className="text-lg mb-4">Com o nosso sistema, você poderá realizar simulações de empréstimo direto no site! Após isso poderá falar com algum de nossos consultores.</p>
+          <Button>Iniciar simulação</Button>
+        </div>
+        <img className="md:w-1/2 md:h-1/2 mb-4" src={HomeSvg} alt="Home SVG" height={400} width={400}/>
+      </section>
+
+    {/* Sessão 2 */}
+    <section className="w-full bg-gradient-to-r from-white to-blue-200">
+    <h2>Nossos serviços</h2>
+      
     </section>
+    </>
   )
 }
