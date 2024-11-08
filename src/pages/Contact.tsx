e4f175941bd91306a7f894a76719e3b8ae5558e2
Dog1337s <anacarolinamarketdigital@gmail.com>
2024-11-08 21:57:47 +0000

    Add: ShadCN Form and Contact SVG

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,11 @@
-
+import ContactSVG from '../assets/Contact.svg'
 
 export default function Contact() {
   return (
-    <h1>Contato</h1>
+    <section>
+      <img src={ContactSVG} alt="Contact SVG"/>
+      <h2>Fale Conosco</h2>
+      
+    </section>
   )
 }
