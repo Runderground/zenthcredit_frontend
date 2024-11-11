f060a69a660c21596b77e88861b0725b038afadf
Dog1337s <anacarolinamarketdigital@gmail.com>
2024-11-11 21:45:44 +0000

    dev: Developing Loan simulator page

--- src/pages/SimuladorDeEmprestimo.tsx
+++ src/pages/SimuladorDeEmprestimo.tsx
@@ -1,4 +1,8 @@
 import {
+  useState
+} from 'react'
+
+import {
   Card,
   CardHeader,
   CardTitle,
@@ -10,29 +14,135 @@ import {
 } from 'lucide-react'
 
 import {
-  Input
-} from '@/components/ui/input'
+  Slider
+} from '@/components/ui/slider'
 
 import {
-  
-}
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectItem
+} from '@/components/ui/select'
 
+import {
+  Button
+} from '@/components/ui/button'
 
 export default function SimuladorDeEmprestimo() {
+  let [valor, setValor] = useState(0);
+  let [meses, setMeses] = useState(0);
+  let [juros, setJuros] = useState("0")
+
+  const handleSliderChange = (newValue: number[]) => {
+    setValor(newValue[0]);
+  }
+
+  const handleMesesChange = (newValue: number[]) => {
+    setMeses(newValue[0])
+  }
+
+  function calcularParcela(valorTotal: number, taxaJuros: string, parcelas: number) {
+    if(valorTotal === 0 || Number(taxaJuros) === 0 || parcelas === 0) {
+      return 0
+    }
+    const taxaMensal = Number(taxaJuros) / 100 / 12;
+
+    const parcela = valorTotal * (taxaMensal * Math.pow(1 + taxaMensal, parcelas)) / (Math.pow(1 + taxaMensal, parcelas) - 1);
+
+    return parcela.toFixed(2)
+  }
+
+  let parcelas = calcularParcela(valor, juros, meses)
+
+  function calcularTotalComJuros(valorComJuros: number, parcelas: number) {
+    const total = valorComJuros * parcelas
+    return total.toFixed(2).toString()
+  }
+
+  let TotalComJuros = calcularTotalComJuros(parcelas, meses)
+  
   return (
     <section className="m-4 mt-12 flex flex-col items-center">
-      <div className="text-center">
-        <h1 className="text-2xl font-bold text-slate-600">Simulador de Empréstimo</h1>
+
+      {/* Titulo */}
+      <div className="mb-4">
+          <h1 className="text-2xl font-bold text-slate-600">Simulador de Empréstimo</h1>
         <p className="text-lg font-normal text-slate-400">Isto é apenas um simulador, não quer dizer que irá ser o valor definitivo de um futuro empréstimo.</p>
       </div>
-      <div className="w-full">
+
+      {/* Cards */}
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
         <Card>
-          <CardHeader className="flex flex-row items-center justify-between">
-            <CardTitle>Empréstimo</CardTitle>
+          <CardHeader className="flex flex-row items-center justify-between text-slate-600">
+            <CardTitle>Como será seu <strong className="text-blue-500">empréstimo?</strong></CardTitle>
             <CircleDollarSign/>
           </CardHeader>
-          <CardContent>
-          
+          <CardContent className="flex flex-col gap-4">
+
+            {/* Valor do empréstimo */}
+            <div className="flex flex-col gap-4">
+              <span className="text-md text-slate-500 font-semibold">Valor do empréstimo:</span>
+              <Slider onValueChange={handleSliderChange} max={100000} step={valor < 15000 ?1000 : 5000}/>
+            <div className="flex justify-between w-full text-sm text-slate-300">
+            <span>R$ 1000</span>
+            <span>R$ 100.000</span>
+            </div>
+            </div>
+            <h2 className="text-slate-700 text-3xl text-center font-bold">{valor.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</h2>
+
+            {/* Meses / Tempo de Pagamento */}
+            <div className="flex flex-col gap-4">
+              <span className="text-md text-slate-500 font-semibold">Tempo de Pagamento:</span>
+              <Slider onValueChange={handleMesesChange} max={120} step={meses < 12 ? 6 : 12}/>
+            <div className="flex justify-between w-full text-sm text-slate-300">
+            <span>6 meses</span>
+            <span>120 meses</span>
+            </div>
+            </div>
+            <h2 className="text-slate-700 text-3xl text-center font-bold">{meses} meses</h2>
+
+
+            {/* Valor do Juros */}
+            <div className="flex flex-col gap-4">
+              <span className="text-md text-slate-500 font-semibold">Valor do Juros:</span>
+              <Select onValueChange={(value) => setJuros(value)}>
+                <SelectTrigger>
+                  <SelectValue placeholder="Quantos %?"/>
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="1.09">1,09% ( com garantia de Ímovel )</SelectItem>
+                  <SelectItem value="1.49">1,49% ( com garantia de Veículo )</SelectItem>
+                  <SelectItem value="7.95">7,95% ( média Emprestimo Pessoal )</SelectItem>
+                </SelectContent>
+              </Select>
+            <div className="flex justify-between w-full text-sm text-slate-300">
+            <span>Não é valor definitivo, pode surgir qualquer alteração ao fazer uma consulta.</span>
+            </div>
+            </div>
+            <h2 className="text-slate-700 text-3xl text-center font-bold">{juros}%</h2>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="text-slate-600">
+            <CardTitle>Resultado</CardTitle>
+          </CardHeader>
+          <CardContent className="flex flex-col items-center justify-center">
+            <h1 className="text-2xl font-semibold text-slate-600 text-center">Pagamento Mensal</h1>
+            <h2 className="text-4xl font-bold text-blue-500"> {parcelas.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</h2>
+            <span className="text-slate-300 font-semibold text-sm">taxas de juros incluídos</span>
+            <div className="flex w-full justify-between">
+              
+            <div className="flex flex-col items-center bg-slate-200 p-4 rounded-lg">
+              <span className="text-sm font-semibold text-slate-400">Valor do empréstimo</span>
+              <h2 className="text-3xl text-slate-600 text-center font-bold">{valor.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</h2>
+            </div>
+
+              <div className="flex flex-col items-center bg-slate-200 p-4 rounded-lg">
+                <span className="text-sm font-semibold text-slate-400">Valor do empréstimo</span>
+                <h2 className="text-3xl text-slate-600 text-center font-bold">{TotalComJuros.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</h2>
+              </div>
+            </div>
           </CardContent>
         </Card>
       </div>
