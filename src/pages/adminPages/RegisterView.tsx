3e19eb481f6825b3e7836581c9c865aa202188bc
OnKzinho1 <onkzovendas@gmail.com>
2024-11-23 21:36:48 +0000

    dev: modo edição

--- src/pages/adminPages/RegisterView.tsx
+++ src/pages/adminPages/RegisterView.tsx
@@ -4,9 +4,9 @@ import axios from "axios";
 
 import { Link, useParams, useNavigate } from "react-router-dom";
 
-import { Card, CardTitle, CardHeader, CardContent } from "@/components/ui/card";
+import { Card, CardTitle, CardHeader, CardContent, CardDescription } from "@/components/ui/card";
 
-import { ChevronLeft, Trash, Pencil } from "lucide-react";
+import { ChevronLeft, Trash, Pencil, UserRoundCheck } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import toast from "react-hot-toast";
 
@@ -34,6 +34,7 @@ export default function RegisterView() {
   }
   
   const deleteUser = async (id: string) => {
+    setDeleteLoad(true)
       try {
         const { data } = await axios.delete(`${import.meta.env.VITE_API_URL}/cadastros/delete/${id}`, {
           headers: {
@@ -41,13 +42,17 @@ export default function RegisterView() {
           },
         },)
         toast.success(data.success)
+        setDeleteLoad(false)
         navigate("../admin/cadastros/", {replace: true})
       } catch (error) {
+        setDeleteLoad(false)
         console.log(error)
       }
   }
   
   const location = useParams();
+  const [deleteLoad, setDeleteLoad] = useState(false)
+  const [editMode, setEditMode] = useState(true)
   const [user, setUser] = useState({
     _id: "",
     nome: "",
@@ -104,7 +109,7 @@ export default function RegisterView() {
 
   return (
     <>
-      <section className="m-4 mt-20">
+      {!editMode && <section className="m-4 mt-20">
         <div className="flex gap-4 items-center mb-4">
           <Link to="/admin/cadastros">
             <ChevronLeft />
@@ -116,25 +121,29 @@ export default function RegisterView() {
             <CardTitle className="text-lg text-blue-500">
               ID: {user._id}
             </CardTitle>
-            <Dialog>
-              <DialogTrigger>
-                <Button
-                className="bg-red-400 hover:bg-red-500"
-              >
-                <Trash />
-              </Button>
-              </DialogTrigger>
-              <DialogContent>
-                <DialogHeader>
-                  <DialogTitle>Tem certeza que deseja <strong className="text-red-500">Deletar?</strong></DialogTitle>
-                  <DialogDescription>
-                    Você tem certeza que gostaria de deletar <strong>{user.nome}</strong> dos cadastros?
-                  </DialogDescription>
-                </DialogHeader>
-                  <Button className="bg-red-400 hover:bg-red-500"
-                    onClick={() => deleteUser(user._id)}>Deletar</Button>
-              </DialogContent>
-            </Dialog>
+            <div className="flex gap-2">
+              <Button className="bg-yellow-500 hover:bg-yellow-600"><Pencil/></Button>
+              <Dialog>
+                <DialogTrigger>
+                  <Button
+                  className="bg-red-400 hover:bg-red-500"
+                >
+                  <Trash />
+                </Button>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>Tem certeza que deseja <strong className="text-red-500">Deletar?</strong></DialogTitle>
+                    <DialogDescription>
+                      Você tem certeza que gostaria de deletar <strong>{user.nome}</strong> dos cadastros?
+                    </DialogDescription>
+                  </DialogHeader>
+                    <Button className="bg-red-400 hover:bg-red-500"
+                      onClick={() => deleteUser(user._id)}
+                      disabled={deleteLoad}>{deleteLoad ? "Aguarde..." : "Deletar"}</Button>
+                </DialogContent>
+              </Dialog>
+            </div>
           </CardHeader>
           <CardContent className="grid grid-cols-1 lg:grid-cols-2 gap-8">
             <div className="flex flex-col gap-2">
@@ -212,9 +221,14 @@ export default function RegisterView() {
         </Card>
         <br/>
         <Card>
-          <CardHeader className="flex flex-row justify-between items-center">
-              <CardTitle>Verificação de Identidade</CardTitle>
-              <Button className="bg-yellow-500 hover:bg-yellow-600"><Pencil/></Button>
+          <CardHeader className="flex flex-col">
+              <div className="flex flex-row justify-between items-center">
+                <CardTitle>Verificação de Identidade</CardTitle>
+                <UserRoundCheck/>
+              </div>
+            <CardDescription>
+              Para ampliar as imagens/documentos, basta clica-las!
+            </CardDescription>
           </CardHeader>
           <CardContent>
             <div className="flex flex-col justify-center m-4 items-center">
@@ -256,10 +270,155 @@ export default function RegisterView() {
                 </div>
               </div>
             </div>
-            <span>{user?.documentos[0].residencia[0].url}</span>
           </CardContent>
         </Card>
-      </section>
+      </section>}
+
+      {/* Modo edição */}
+      {editMode && <section className="m-4 mt-20">
+          <div className="flex gap-4 items-center mb-4">
+            <Link to="/admin/cadastros">
+              <ChevronLeft />
+            </Link>
+            <h1 className="text-2xl font-semibold">Consulta de cadastro</h1>
+          </div>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between">
+              <CardTitle className="text-lg text-blue-500">
+                ID: {user._id}
+              </CardTitle>
+              <div className="flex gap-2">
+                <h2>MODO EDIÇÃO ATIVADO</h2>
+              </div>
+            </CardHeader>
+            <CardContent className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+              <div className="flex flex-col gap-2">
+                <h3 className="text-lg font-bold text-slate-400">
+                  Informações Gerais
+                </h3>
+                <span>
+                  <strong className="font-semibold text-slate-600">Nome :</strong>{" "}
+                  {user.nome}
+                </span>
+                <span>
+                  <strong className="font-semibold text-slate-600">
+                    Email :
+                  </strong>{" "}
+                  {user.email}
+                </span>
+                <span>
+                  <strong className="font-semibold text-slate-600">
+                    Telefone :
+                  </strong>{" "}
+                  {user.telefone}
+                </span>
+                <span>
+                  <strong className="font-semibold text-slate-600">CPF :</strong>{" "}
+                  {user.cpf}
+                </span>
+                <span>
+                  <strong className="font-semibold text-slate-600">
+                    Nascimento :
+                  </strong>{" "}
+                  {user.nascimento}
+                </span>
+                <span>
+                  <strong className="font-semibold text-slate-600">CEP :</strong>{" "}
+                  {user.cep}
+                </span>
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <h3 className="text-lg font-bold text-slate-400">
+                  Análise de Perfil
+                </h3>
+                <span>
+                  <strong className="font-semibold text-slate-600">
+                    Renda Mensal :
+                  </strong>{" "}
+                  {user.renda}
+                </span>
+                <span>
+                  <strong className="font-semibold text-slate-600">
+                    Ocupação :
+                  </strong>{" "}
+                  {user.ocupacao}
+                </span>
+                <span>
+                  <strong className="font-semibold text-slate-600">
+                    Motivo do empréstimo :
+                  </strong>{" "}
+                  {user.motivo}
+                </span>
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <h3 className="text-lg font-bold text-slate-400">
+                  Análise de Garantia
+                </h3>
+                <span>
+                  <strong className="font-semibold text-slate-600">
+                    Possui garantia :
+                  </strong>{" "}
+                  {user.garantia}
+                </span>
+              </div>
+            </CardContent>
+          </Card>
+          <br/>
+          <Card>
+            <CardHeader className="flex flex-col">
+                <div className="flex flex-row justify-between items-center">
+                  <CardTitle>Verificação de Identidade</CardTitle>
+                  <UserRoundCheck/>
+                </div>
+              <CardDescription>
+                Para ampliar as imagens/documentos, basta clica-las!
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="flex flex-col justify-center m-4 items-center">
+                <div className="flex flex-col gap-12 w-full">
+                  <div className="flex flex-col items-center text-blue-400 font-semibold text-lg lg:text-3xl">
+                    <span className="mb-2">Identidade</span>
+                    {containsPdf(user?.documentos[0].identidade[0].url) ? (
+                      <div className="flex flex-col items-center justify-center cursor-pointer w-[200px] h-[200px] bg-red-400 hover:bg-red-500 transition-colors rounded-lg" onClick={() => {window.open(`${user?.documentos[0].identidade[0].url}`)}}>
+                        <div className="flex flex-col items-center justify-center select-none">
+                          <h2 className="text-3xl text-white">PDF</h2>
+                          <span className="text-xs text-white">Clique para visualizar</span>
+                        </div>
+                      </div>
+                    ) : <img className="rounded-lg lg:w-72 cursor-pointer" onClick={() => {window.open(`${user?.documentos[0].identidade[0].url}`)}} src={user?.documentos[0].identidade[0].url} width={150} height={200} alt="Comprovante de Residencia"/>}
+
+                  </div>
+                  <div className="flex flex-col text-lg lg:text-3xl items-center text-blue-400 font-semibold">
+                    <span className="mb-2">Comprovante de Renda</span>
+                    {containsPdf(user?.documentos[0].comprovante_renda[0].url) ? (
+                      <div className="flex flex-col items-center justify-center cursor-pointer w-[200px] h-[200px] bg-red-400 hover:bg-red-500 transition-colors rounded-lg" onClick={() => {window.open(`${user?.documentos[0].comprovante_renda[0].url}`)}}>
+                        <div className="flex flex-col items-center justify-center select-none">
+                          <h2 className="text-3xl text-white">PDF</h2>
+                          <span className="text-xs text-white">Clique para visualizar</span>
+                        </div>
+                      </div>
+                    ) : <img className="rounded-lg lg:w-72 cursor-pointer" onClick={() => {window.open(`${user?.documentos[0].comprovante_renda[0].url}`)}} src={user?.documentos[0].comprovante_renda[0].url} width={150} height={200} alt="Comprovante de Residencia"/>}
+
+                  </div>
+                  <div className="flex flex-col text-lg lg:text-3xl items-center text-blue-400 font-semibold">
+                    <span className="mb-2">Comprovante de Residência</span>
+                    {containsPdf(user?.documentos[0].residencia[0].url) ? (
+        <div className="flex flex-col items-center justify-center cursor-pointer w-[200px] h-[200px] bg-red-400 hover:bg-red-500 transition-colors rounded-lg" onClick={() => {window.open(`${user?.documentos[0].residencia[0].url}`)}}>
+          <div className="flex flex-col items-center justify-center select-none">
+            <h2 className="text-3xl text-white">PDF</h2>
+            <span className="text-xs text-white">Clique para visualizar</span>
+          </div>
+        </div>
+        ) : <img className="rounded-lg lg:w-72 cursor-pointer" onClick={() => {window.open(`${user?.documentos[0].residencia[0].url}`)}} src={user?.documentos[0].residencia[0].url} width={150} height={200} alt="Comprovante de Residencia"/>}
+                  </div>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+        </section>}
     </>
   );
 }
